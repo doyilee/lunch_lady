248f8f7bc2c9d1ed5ea2e9909c2596f4c3244db6
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-05-28 16:38:37 -0600

    user_choice added

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,10 +1,21 @@
 class LunchLady
 	def initialize 
 		@main_dish = {
-			'Tuna fish sandwich' => 2, 'Pizza' => 3, 'Korean BBQ' => 5
+			'(1)Tuna fish sandwich' => 2, '(2)Pizza' => 3, '(3)Korean BBQ' => 5
 		}
 		@side_dish = {
-			'Salad' => 1, 'Coleslaw' => 1, 'Kimchi' => 1
+			'(1)Salad' => 1, '(2)Coleslaw' => 1, '(3)Kimchi' => 1
 		}
 	end
+
+  def user_choice
+    puts "Welcome to the lunch room?"
+    puts "Pick a main dish: #{@main_dish}"
+    puts "Type dish number"
+    @user_choice = gets.chomp
+  end
+
 end
+
+clone = LunchLady.new
+clone.user_choice
