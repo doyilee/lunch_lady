afad7470365f7798f7893aea96ce647a94c712bb
Doyi Lee <doyilee@hotmail.com>
2015-05-28 16:17:58 -0600

    created dishes

---
+++ lunch_lady.rb
@@ -0,0 +1,10 @@
+class LunchLady
+	def initialize 
+		@main_dish = {
+			'Tuna fish sandwich' => 2, 'Pizza' => 3, 'Korean BBQ' => 5
+		}
+		@side_dish = {
+			'Salad' => 1, 'Coleslaw' => 1, 'Kimchi' => 1
+		}
+	end
+end
